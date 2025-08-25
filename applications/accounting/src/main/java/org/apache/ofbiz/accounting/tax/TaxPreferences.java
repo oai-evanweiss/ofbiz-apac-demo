4c321fbe37ad875c01412b74a21537572a96c899
oai-evanweiss <150647345+oai-evanweiss@users.noreply.github.com>
2025-08-25 10:49:13 -0700

    Update TaxPreferences.java

--- applications/accounting/src/main/java/org/apache/ofbiz/accounting/tax/TaxPreferences.java
+++ applications/accounting/src/main/java/org/apache/ofbiz/accounting/tax/TaxPreferences.java
@@ -1,3 +1,11 @@
-public static boolean isTaxIncludedForGeo(String geoId, Boolean taxInPrice) {
-    return taxInPrice != null && taxInPrice;
+public final class TaxPreferences {
+    private TaxPreferences() {}
+
+    /**
+     * Returns true if tax is already included in the price.
+     * No geo-specific overrides here.
+     */
+    public static boolean isTaxIncludedForGeo(String geoId, Boolean taxInPrice) {
+        return Boolean.TRUE.equals(taxInPrice);
+    }
 }
